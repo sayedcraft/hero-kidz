70a750995032bc2bcc1650eaa03fd53c80dfa28a
Sayed <muhammadsaye741@gmail.com>
2026-08-27 23:29:58 +0600

    73.3

---
+++ src/app/products/page.jsx
@@ -0,0 +1,12 @@
+import Products from '@/component/home/Products';
+import React from 'react';
+
+const ProductPage = () => {
+    return (
+        <div>
+            <Products></Products>
+        </div>
+    );
+};
+
+export default ProductPage;
